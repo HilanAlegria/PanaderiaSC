79057ba81267b81c42cf00df3040121d976546b7
HilanAlegria <hilan.alegria00@usc.edu.co>
2025-05-18 09:05:03 -0500

    Delete API-Panaderia-SC directory

--- API-Panaderia-SC/src/main/java/com/example/API_Panaderia/SC/repository/ProductoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.API_Panaderia.SC.repository;
-
-import com.example.API_Panaderia.SC.model.Producto; 
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductoRepository extends MongoRepository<Producto, String> {
-   
-}
